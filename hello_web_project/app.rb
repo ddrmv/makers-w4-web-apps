ee13a7635ce25bf19a619fc38bd281781b383690
D. Dramchev <ddramchev@gmail.com>
2022-09-12 20:58:06 +0100

    setup hello_web_project and complete phase2 bite2 bite3

---
+++ hello_web_project/app.rb
@@ -0,0 +1,46 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Application < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    return "Hello"
+  end
+
+  get '/greet' do
+    return params[:name] * params[:times].to_i
+  end
+
+  get '/names' do
+    return "Julia, Mary, Karim"
+  end
+
+  post '/sort-names' do
+    names = params[:names].split(',')
+    sorted_names = names.sort.join(',')
+    return sorted_names
+  end
+
+  # get '/' do
+  #   name = params[:name]
+  #   return "Hello #{name}"
+  # end
+
+  # post '/hello' do
+  #   name = params[:name]
+  #   return "Hello #{name}"
+  # end
+
+  # get '/hello' do
+  #   return "Hello #{params[:name]}"
+  # end
+
+  # post '/submit' do
+  #   name = params[:name]
+  #   message = params[:message]
+  #   return "Thanks #{name}, you sent this message: \"#{message}\""
+  # end
+end
---
+++ hello_web_project/spec/integration/app_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+require 'rack/test'
+require_relative '../../app'
+
+RSpec.describe Application do
+  include Rack::Test::Methods
+  let(:app) { Application.new }
+
+  context "GET /" do
+    it "returns 200 with the right content" do
+      response = get('/')
+
+      expect(response.status).to eq 200
+      expect(response.body).to eq "Hello"
+    end
+
+    it "returns 200 with the right content" do
+      response = get('/greet', name: "Dana", times: 2)
+      expect(response.status).to eq 200
+      expect(response.body).to eq "DanaDana"
+    end
+  end
+
+  context "GET /names" do
+    it "returns 200 with body of three names" do
+      response = get('/names')
+      expect(response.status).to eq 200
+      expect(response.body).to eq "Julia, Mary, Karim"
+    end
+  end
+
+  context "POST /sort-names" do
+    it "returns 200 with sorted names" do
+      response = post('/sort-names', names: "Joe,Alice,Zoe,Julia,Kieran")
+      expect(response.status).to eq 200
+      expect(response.body).to eq "Alice,Joe,Julia,Kieran,Zoe"
+    end
+  end
+  
+end
